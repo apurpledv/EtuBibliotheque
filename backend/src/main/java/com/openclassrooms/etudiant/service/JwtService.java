5918dfd035d312f4cf088b623e5c2e5524941b0c
apurpledv <david.chicot.77@gmail.com>
2026-01-14 15:05:17 +0100

    fixed API login: it now properly validates the user's credentials and generates a JWT

--- backend/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ backend/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,23 @@
 package com.openclassrooms.etudiant.service;
 
+import java.security.Key;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 @Service
 public class JwtService {
+    private final Key jwtKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(UserDetails userDetails) {
-        return null; // TODO
+        return Jwts.builder()
+            .setSubject(userDetails.getUsername())
+            .signWith(jwtKey)
+            .compact();
     }
 
 }
